3f13fc4343204a36985847aceedfd7a01b731d12
GroverTB <grover.jtb@gmail.com>
2016-12-02 23:38:40 -0500

    [ADD] ETAPAS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Inicio' });
+});
+
+router.get('/nosotros', function(req, res, next) {
+  res.render('nosotros', { title: 'Nosotros' });
+});
+
+router.get('/etapa1', function(req, res, next) {
+  res.render('etapa1', { title: 'Etapa 1' });
+});
+
+router.get('/etapa2', function(req, res, next) {
+  res.render('etapa2', { title: 'Etapa 2' });
+});
+
+router.get('/etapa3', function(req, res, next) {
+  res.render('etapa3', { title: 'Etapa 3' });
+});
+
+router.get('/producto', function(req, res, next) {
+  res.render('producto', { title: 'Producto' });
+});
+
+router.get('/contactenos', function(req, res, next) {
+  res.render('contactenos', { title: 'Contáctenos' });
 });
 
 module.exports = router;
